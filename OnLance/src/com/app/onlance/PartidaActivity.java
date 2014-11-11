0d71314d7a6fa055e565689f56f6d181cc6f1c8d
gdss <gdss@cin.ufpe.br>
2014-11-11 13:20:30 +0000

    primeira impressão, layout partida

--- OnLance/src/com/app/onlance/PartidaActivity.java
+++ OnLance/src/com/app/onlance/PartidaActivity.java
@@ -17,64 +17,66 @@ public class PartidaActivity extends Activity {
 
 	Chronometer cronometro;
 	boolean teste = true;
-	
-	@Override
-    protected void onCreate(Bundle savedInstanceState) {
-	 super.onCreate(savedInstanceState);
-     setContentView(R.layout.partida);
-     
-     List<String> jogadore = new ArrayList<String>();
-     List<String> jogadore2 = new ArrayList<String>();
-     
-     
-     jogadore.add("Kaio");
-     jogadore.add("João");
-     jogadore.add("zé");
-     jogadore.add("geyson");
 
-     jogadore2.add("Kaio");
-     jogadore2.add("João");
-     jogadore2.add("zé");
-     jogadore2.add("geyson");
-     
-     
-     ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,jogadore);
-     ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,jogadore);
-     
-     ListView view = (ListView) findViewById(R.id.time1);
-     ListView view2 = (ListView) findViewById(R.id.time2);
-     
-     view.setAdapter(adapter);
-     view2.setAdapter(adapter2);
-     cronometro = (Chronometer) findViewById(R.id.chronometer1);
-     cronometro.setOnChronometerTickListener(new OnChronometerTickListener() {
-		
-		@Override
-		public void onChronometerTick(Chronometer arg0) {
-			
-			String limite = "00:20";
-			
-			String valorCronometro = arg0.getText().toString();
-			if(valorCronometro.equals(limite)){
-				Toast.makeText(PartidaActivity.this, "fim", Toast.LENGTH_LONG).show();
-				cronometro.stop();
-				
-			}
-		}
-	});
-     
-     
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.partida);
+
+		List<String> jogadores1 = new ArrayList<String>();
+		List<String> jogadores2 = new ArrayList<String>();
+
+		jogadores1.add("Geyson");
+		jogadores1.add("Kaio");
+		jogadores1.add("Zé Carlos");
+		jogadores1.add("João Vitor");
+
+		jogadores2.add("Helton");
+		jogadores2.add("3xato");
+		jogadores2.add("Natan");
+		jogadores2.add("Tertius");
+
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, jogadores1);
+		ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, jogadores2);
+
+		ListView view = (ListView) findViewById(R.id.time1);
+		ListView view2 = (ListView) findViewById(R.id.time2);
+
+		view.setAdapter(adapter);
+		view2.setAdapter(adapter2);
+		cronometro = (Chronometer) findViewById(R.id.chronometer1);
+		cronometro.setOnChronometerTickListener(new OnChronometerTickListener() {
+
+					@Override
+					public void onChronometerTick(Chronometer arg0) {
+
+						String limite = "00:20";
+
+						String valorCronometro = arg0.getText().toString();
+						if (valorCronometro.equals(limite)) {
+							Toast.makeText(PartidaActivity.this, "fim",
+									Toast.LENGTH_LONG).show();
+							cronometro.stop();
+
+						}
+					}
+				});
+
 	}
-	
-	public void start(View view){
+
+	public void start(View view) {
 		cronometro.setBase(SystemClock.elapsedRealtime());
 		cronometro.start();
-		if(teste){
-			Toast.makeText(this, String.valueOf(cronometro.getBase()), Toast.LENGTH_LONG).show();
-			teste =false;
-		}else{
-			Toast.makeText(this, cronometro.getText(), Toast.LENGTH_LONG).show();
-			teste =true;
+		if (teste) {
+			Toast.makeText(this, String.valueOf(cronometro.getBase()),
+					Toast.LENGTH_LONG).show();
+			teste = false;
+		} else {
+			Toast.makeText(this, cronometro.getText(), Toast.LENGTH_LONG)
+					.show();
+			teste = true;
 		}
 	}
 }
