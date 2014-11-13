dc2d35d0866092abca596eca2fe391c2e5691612
jvba@cin.ufpe.br <jvba@cin.ufpe.br@1d433b6c-1e36-7dad-54cf-9ee4c9439db7>
2014-11-13 00:45:40 +0000

    JVBA - Tela de Seleção Jogador/Time

---
+++ trunk/OnLance/src/com/app/onlance/JogadorFragment.java
@@ -0,0 +1,21 @@
+package com.app.onlance;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class JogadorFragment extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+
+		View rootView = inflater.inflate(R.layout.jogador_fragment, container, false);
+		
+		return rootView;
+	}
+
+}
---
+++ trunk/OnLance/src/com/app/onlance/TimeFragment.java
@@ -0,0 +1,20 @@
+package com.app.onlance;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class TimeFragment extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+
+		View rootView = inflater.inflate(R.layout.time_fragment, container, false);
+		
+		return rootView;
+	}
+}
