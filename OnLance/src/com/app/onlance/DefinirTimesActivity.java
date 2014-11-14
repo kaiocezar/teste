12784addf62e75125af05ff607c5108c13982131
kcpo <kcpo@cin.ufpe.br>
2014-11-14 22:35:13 +0000

    [kcpo] click com cor no fundo do listview

--- OnLance/src/com/app/onlance/DefinirTimesActivity.java
+++ OnLance/src/com/app/onlance/DefinirTimesActivity.java
@@ -1,19 +1,19 @@
 package com.app.onlance;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.SimpleAdapter;
 
 public class DefinirTimesActivity extends Activity {
 
@@ -22,63 +22,46 @@ public class DefinirTimesActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.definir_times);
 
-		List<HashMap<String, Object>> jogadores = new ArrayList<HashMap<String, Object>>();
+		final List<JogadorForList> jogadores = new ArrayList<JogadorForList>();
 
 		for (int i = 0; i < 20; i++) {
-			HashMap<String, Object> mapa = new HashMap<String, Object>();
-			mapa.put("nome", "kaio" + i);
-			mapa.put("tipoTela", "0");
+			JogadorForList mapa = new JogadorForList();
+			mapa.setNome("kaio" + i);
+			mapa.setTipoTela("0");
 
 			jogadores.add(mapa);
 		}
 
-		String[] from = new String[] { "nome", "tipoTela" };
 
-		int[] to = new int[] { R.id.textPartida, R.id.tipoTelaNum };
-
-		SimpleAdapterLance adap = new SimpleAdapterLance(this, jogadores,
-				R.layout.layout_list_time, from, to);
-
-		
 		ListView view = (ListView) findViewById(R.id.jogadores);
 
-		view.setAdapter(adap);
+		view.setAdapter(new BaseAdapterOnLance(this, jogadores));
 		
-		view.setOnItemSelectedListener(new OnItemSelectedListener() {
+		view.setOnItemClickListener(new OnItemClickListener() {
 
 			@Override
-			public void onItemSelected(AdapterView<?> arg0, View arg1,
-					int arg2, long arg3) {
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+					long arg3) {
+				LinearLayout lay = (LinearLayout) arg1.findViewById(R.id.content);
 				
-				EditText tipoTela = (EditText) arg1
-						.findViewById(R.id.tipoTelaNum);
+				JogadorForList joga = jogadores.get(arg2);
+				if (joga.getTipoTela().equals("0")) {
 
-				if (tipoTela.getText().toString().equals("0")) {
+					 lay.setBackgroundColor(Color.RED);
+					joga.setTipoTela("1");
 
-					arg0.getChildAt(arg2).setBackgroundColor(Color.RED);
-					// arg1.setBackgroundColor(Color.RED);
-					tipoTela.setText("1");
-
-				} else if (tipoTela.getText().toString().equals("1")) {
-					arg0.getChildAt(arg2).setBackgroundColor(Color.BLUE);
-					// arg1.setBackgroundColor(Color.BLUE);
-					tipoTela.setText("2");
+				} else if (joga.getTipoTela().equals("1")) {
+					 lay.setBackgroundColor(Color.BLUE);
+					joga.setTipoTela("2");
 				} else {
-					arg0.getChildAt(arg2).setBackgroundColor(Color.WHITE);
-					// arg1.setBackgroundColor(Color.WHITE);
-					tipoTela.setText("0");
+					 lay.setBackgroundColor(Color.WHITE);
+					joga.setTipoTela("0");
 				}
 
-				
 			}
-
-			@Override
-			public void onNothingSelected(AdapterView<?> arg0) {
-				
-			}
-			
-			
 		});
+		
+		
 		
 
 	}
